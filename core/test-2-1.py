a2d5f1f769c29f13daac183f0ad7fd2dc77c5030
davidwood3 <davidwood3@gmail.com>
2024-10-28 15:29:23 -0700

    First Commit

---
+++ core/test-2-1.py
@@ -0,0 +1,19 @@
+# https://github.com/ax-va/PyOpenGL-Pygame-Stemkoski-Pascale-2021/tree/main
+
+from core.base import Base
+
+class Test(Base):
+
+    def initialize(self):
+        pass
+        # print("Initialize")
+
+    def update(self):
+        pass
+    #  print("Update")
+
+    # def render(self):
+    #    print("Render")
+
+# instantiate the class and run the program
+Test().run()
